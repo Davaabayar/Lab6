29fae20b899dadccc87d88fa0fbec04abae50a16
Davaabayar <busdavaabayar@gmail.com>
2019-11-06 10:43:09 -0600

    Comment added

--- js/app.js
+++ js/app.js
@@ -21,22 +21,30 @@ function startTime() {
     }, 500);
 }
 
+//3. Store your startup data as a JSON array e.g. const students = [ { "000-98-0001": "John" }, {"...":"..."} ];
 const students = [
     { sid: "000-98-0001", name: "James" },
     { sid: "000-98-0002", name: "Anna" },
     { sid: "000-98-0003", name: "Jeffrey" }
 ];
 
+//1. IIFE - Use an IIFE to initialize the state of the web page. i.e. wrap your JavaSscript code which contains your event handlers etc, inside an IIFE.
+//Here our initial stage is Time should start ticking and initial const list should be displayed
 (function () {
+    //start time
     startTime();
 
+    //show initial students in const list
     students.forEach(stu => {
         addStudentToList(stu);
     }); 
+
+    //2. Use Unobstrusive JS for Event handler - i.e. use the DOM API call such as, objectRef.addEventListener, to create your event handlers.
     let button = document.getElementById("submitBtn");      
     button.onclick = addNewStudent;
 })();
 
+//4. Use the DOM API calls, document.createElement(...) & listObjectRef.appendChild(...), to dynamically add new Student data to the List Items on the webpage. 
 function addStudentToList(student) {
     let newStudent = document.createElement("li"); 
     let newStudentContent = document.createTextNode(student.sid + " - " + student.name); 
@@ -45,12 +53,15 @@ function addStudentToList(student) {
     document.getElementById("studentlist").appendChild(newStudent);    
 }
 
-function addNewStudent(){
+function addNewStudent(){   
     let sid = document.getElementById("sid");
-    let fname = document.getElementById("name");   
-    let student = {
-        "sid": sid.value,
-        "name": fname.value
-    };
-    addStudentToList(student);   
+    let fname = document.getElementById("name"); 
+
+    if(sid.value !='' && fname.value!=''){ //prevent empty data to be submitted and force pattern check for sid
+        let student = {
+            "sid": sid.value,
+            "name": fname.value
+        };
+        addStudentToList(student);   
+    }
 }
